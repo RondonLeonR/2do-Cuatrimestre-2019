ca23a9d71703bed1e04b83580077a4ce33db9a68
alumno <alumno>
2019-03-19 12:05:39 -0300

    Ejercicios

---
+++ Rondon.Rodolfo2/Ejercicio 01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int numMax = 0;
+            int numMin = 0;
+            int suma = 0;
+            bool flag = true;
+            float promedio;
+
+            Console.Title = "Ejercicio 1";
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write("Ingrese Numero: ");
+                num = int.Parse(Console.ReadLine()) ;
+
+                if(flag == true)
+                {
+                    numMax = num;
+                    numMin = num;
+                    flag = false;
+                }
+
+                if (num > numMax )
+                    numMax = num;
+                else if (num < numMin )
+                    numMin = num;
+                
+                suma = suma +num;
+            }
+
+            promedio = suma / 5.0f;
+            Console.WriteLine("Promedio: {0}\nNumero Maximo: {1}\nNumero Minimo: {2}", promedio,numMax,numMin);
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
---
+++ Rondon.Rodolfo2/Ejercicio02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            double numCuadr;
+            double numCubo;
+            Console.Write("Ingrese Numero: ");
+            num = double.Parse(Console.ReadLine());
+            if(num > 0)
+            {
+                numCuadr = Math.Pow(num, 2);
+                numCubo = Math.Pow(num, 3);
+                Console.WriteLine(numCuadr);
+                Console.WriteLine(numCubo);
+                Console.ReadLine();
+
+
+            }
+            else
+            {
+                Console.Write("ERROR.¡Reingresar Numero!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
