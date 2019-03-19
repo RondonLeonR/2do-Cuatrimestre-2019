1377c82609dd7f2993ba6958a2d715150937616d
alumno <alumno>
2019-03-19 11:04:39 -0300

    1ra Clase

---
+++ Rondon.Rodolfo2/Proyecto01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.Write("Ingrese Nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine(nombre);
+            Console.ReadKey();
+        }
+    }
+}
