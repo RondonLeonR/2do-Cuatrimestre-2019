c0180d5fc09a5b59e8a7053a98967dfed2496ba0
alumno <alumno>
2019-03-20 11:10:09 -0300

    20-03-19

---
+++ Rondon.Rodolfo2/Clase02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sello.mensaje = "aaaaaaaaaa";
+
+            //Console.WriteLine(Sello.Imprimir()); 
+            //Sello.Borrar();
+            Sello.ImprimirEnColor();
+            //Console.WriteLine(Sello.Imprimir());
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Rondon.Rodolfo2/Clase02/Sello.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02
+{
+    public class Sello
+    {
+        public static string mensaje;   //Atributo o Variable de una clase
+        public static ConsoleColor color;
+
+        public static string Imprimir()
+        {
+            
+            return Sello.ArmarFormatoMensaje();
+        }
+
+        public static void Borrar()
+        {
+            Sello.mensaje = "---";
+        }
+
+        public static void ImprimirEnColor()
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(Sello.Imprimir());
+            Console.ForegroundColor = ConsoleColor.Gray;
+            
+
+        }
+
+        private static string ArmarFormatoMensaje()
+        {
+            string mensaje;
+            string mensajeRetorno;
+            int cantCarac;
+
+            cantCarac = Sello.mensaje.Length +2;
+            mensaje = "";
+            mensajeRetorno = "";
+            for(int i=0;i<cantCarac;i++)
+            {
+                mensaje += "*";
+            }
+            mensajeRetorno = mensaje + "\n*" + Sello.mensaje + "*\n" + mensaje;
+
+            return mensajeRetorno;
+        }
+    }
+}
